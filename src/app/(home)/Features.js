191d17d2c20afe415abc8e0e51fa23e2caf6aebe
gogurlbaby <philiahammmond@gmail.com>
2025-03-24 17:24:23 +0000

    updated footer

--- src/app/(home)/Features.js
+++ src/app/(home)/Features.js
@@ -3,75 +3,73 @@ import { Truck, PackageCheck, Gem } from "lucide-react";
 
 function Features() {
   return (
-    <div>
-      <div
-        className="bg-[#01589A]"
-        style={{
-          padding: "2.625rem 2.5rem 7.25rem",
-        }}
+    <div
+      className="bg-[#01589A]"
+      style={{
+        padding: "2.625rem 2.5rem 7.25rem",
+      }}
+    >
+      <p
+        className="lg:text-4xl lg:leading-12 text-white text-2xl font-normal font-sans text-left"
+        style={{ marginBottom: "3.25rem" }}
       >
-        <p
-          className="lg:text-4xl lg:leading-12 text-white text-2xl font-normal font-sans text-left"
-          style={{ marginBottom: "3.25rem" }}
-        >
-          We're tackling the biggest challenges in laptops and electronic
-          products.
-        </p>
+        We're tackling the biggest challenges in laptops and electronic
+        products.
+      </p>
 
-        <div className="lg:grid-cols-3 grid grid-cols-1 gap-[2.5rem]">
-          <div className="flex flex-col justify-center items-center">
-            <Truck
-              size={40}
-              className="text-white text-center"
-              style={{ marginBottom: "1rem" }}
-            />
-            <p
-              className="text-white text-md font-bold font-sans text-center"
-              style={{ marginBottom: "1rem" }}
-            >
-              Fast & free shipping
-            </p>
-            <p className="text-white text-md font-normal font-sans text-center">
-              Every single order ships for free. No minimums, no tiers, no fine
-              print whatsoever.
-            </p>
-          </div>
+      <div className="lg:grid-cols-3 grid grid-cols-1 gap-[2.5rem]">
+        <div className="flex flex-col justify-center items-center">
+          <Truck
+            size={40}
+            className="text-white text-center"
+            style={{ marginBottom: "1rem" }}
+          />
+          <p
+            className="text-white text-md font-bold font-sans text-center"
+            style={{ marginBottom: "1rem" }}
+          >
+            Fast & free shipping
+          </p>
+          <p className="text-white text-md font-normal font-sans text-center">
+            Every single order ships for free. No minimums, no tiers, no fine
+            print whatsoever.
+          </p>
+        </div>
 
-          <div className="flex flex-col justify-center items-center">
-            <PackageCheck
-              size={40}
-              className="text-white text-center"
-              style={{ marginBottom: "1rem" }}
-            />
-            <p
-              className="text-white text-md font-bold font-sans text-center"
-              style={{ marginBottom: "1rem" }}
-            >
-              Innovative, User-Centric Design
-            </p>
-            <p className="text-white text-md font-normal font-sans text-center">
-              Our cutting-edge designs prioritize performance, portability, and
-              seamless integration into your lifestyle.
-            </p>
-          </div>
+        <div className="flex flex-col justify-center items-center">
+          <PackageCheck
+            size={40}
+            className="text-white text-center"
+            style={{ marginBottom: "1rem" }}
+          />
+          <p
+            className="text-white text-md font-bold font-sans text-center"
+            style={{ marginBottom: "1rem" }}
+          >
+            Innovative, User-Centric Design
+          </p>
+          <p className="text-white text-md font-normal font-sans text-center">
+            Our cutting-edge designs prioritize performance, portability, and
+            seamless integration into your lifestyle.
+          </p>
+        </div>
 
-          <div className="flex flex-col justify-center items-center">
-            <Gem
-              size={40}
-              className="text-white text-center"
-              style={{ marginBottom: "1rem" }}
-            />
-            <p
-              className="text-white text-md font-bold font-sans text-center"
-              style={{ marginBottom: "1rem" }}
-            >
-              Durable, High-Quality Materials
-            </p>
-            <p className="text-white text-md font-normal font-sans text-center">
-              We use premium aluminum, high-resolution OLED displays, and
-              durable batteries for superior quality.
-            </p>
-          </div>
+        <div className="flex flex-col justify-center items-center">
+          <Gem
+            size={40}
+            className="text-white text-center"
+            style={{ marginBottom: "1rem" }}
+          />
+          <p
+            className="text-white text-md font-bold font-sans text-center"
+            style={{ marginBottom: "1rem" }}
+          >
+            Durable, High-Quality Materials
+          </p>
+          <p className="text-white text-md font-normal font-sans text-center">
+            We use premium aluminum, high-resolution OLED displays, and durable
+            batteries for superior quality.
+          </p>
         </div>
       </div>
     </div>
--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,19 +4,19 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer style={{ padding: "2.625rem 2.5rem 1.25rem" }}>
-      <hr className="w-full border-[0.5px] border-solid bg-[#D9D9D9]" />
-      <div className="lg:flex-row lg:justify-between flex flex-col items-center bg-white">
-        <div className="flex gap-[1rem] items-center">
-          <img src="/images/visa.svg" alt="visa-logo" />{" "}
-          <img src="/images/paypal.svg" alt="paypal-logo" />
-          <img src="/images/mastercard.svg" alt="mastercard-logo" />
-        </div>
-        <div>
-          <p className="text-black text-md font-normal font-sans">
-            &copy; {currentYear} Evershop. All Rights Reserved.
-          </p>
-        </div>
+    <footer
+      style={{ padding: "2.625rem 2.5rem 1.25rem" }}
+      className="lg:flex-row lg:justify-between w-full border-t-[1px] border-solid border-[#D9D9D9] flex flex-col items-center bg-white"
+    >
+      <div className="flex gap-[1rem] items-center">
+        <img src="/images/visa.svg" alt="visa-logo" />{" "}
+        <img src="/images/paypal.svg" alt="paypal-logo" />
+        <img src="/images/mastercard.svg" alt="mastercard-logo" />
+      </div>
+      <div>
+        <p className="text-black text-md font-normal font-sans">
+          &copy; {currentYear} Evershop. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
