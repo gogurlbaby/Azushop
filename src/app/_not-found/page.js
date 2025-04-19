f5253305b5ee415608a0c9bbb6982a51f6e812c8
gogurlbaby <philiahammmond@gmail.com>
2025-04-19 22:24:04 +0000

    fixed searchparams error at production

--- src/app/_not-found/page.js
+++ src/app/_not-found/page.js
@@ -1,15 +1,19 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { Suspense } from "react";
-import SearchParamsHandler from "./SearchParamsHandler";
+
+export const dynamic = "force-dynamic";
 
 export default function NotFound() {
+  const searchParams = useSearchParams();
+  const auth = searchParams.get("auth");
+
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <Suspense fallback={<div></div>}>
-        <SearchParamsHandler />
-      </Suspense>
+      {auth === "login" && <p>Please log in to continue.</p>}
       <Link href="/" style={{ color: "#01589A", textDecoration: "underline" }}>
         Go back home
       </Link>
